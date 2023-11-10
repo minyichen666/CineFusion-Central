3552fe4f6ca354ac02660e8804e96e792b3c8c27
“minyichen666” <m770706946@gmail.com>
2023-11-10 15:23:17 -0500

    connect to database

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = mysql.connector.connect(
+        host='localhost',  # If Flask isn't running in a Docker container, use Docker's IP here
+        port=3306,
+        database='mysql',
+        user='root',
+        password='rootpassword'
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT NOW()")
+    current_time = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return f"Current time in MySQL: {current_time}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ create-table.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE movie;
+USE movie;
+CREATE TABLE Movie (
+    movie_id INT PRIMARY KEY NOT NULL,
+    type VARCHAR(255),
+    title VARCHAR(255),
+    country VARCHAR(255),
+    date_added DATE,
+    releasing_year INT,
+    duration INT,
+    CHECK (YEAR(date_added) >= releasing_year),
+    CHECK (releasing_year BETWEEN 1800 AND YEAR(CURRENT_DATE)),
+    CHECK (duration > 0 AND duration <= 600)
+);
+
+CREATE TABLE Actor (
+    actor_id INT PRIMARY KEY NOT NULL,
+    actor_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Director (
+    director_id INT PRIMARY KEY NOT NULL,
+    director_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE RatingPlatform (
+    platform_id INT PRIMARY KEY NOT NULL,
+    platform_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE User (
+    Username VARCHAR(255) PRIMARY KEY NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_description VARCHAR(255),
+    user_attributes VARCHAR(255),
+    num_of_followers INT
+);
+
+CREATE TABLE Watchlist (
+    Username VARCHAR(255),
+    ID INT,
+    PRIMARY KEY (Username, ID),
+    FOREIGN KEY (Username) REFERENCES User(Username),
+    UNIQUE (ID)
+);
+
+-- Define relation sets
+CREATE TABLE ActsIn (
+    movie_id INT,
+    actor_id INT,
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+    FOREIGN KEY (actor_id) REFERENCES Actor(actor_id)
+);
+
+CREATE TABLE Directs (
+    movie_id INT,
+    director_id INT,
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+    FOREIGN KEY (director_id) REFERENCES Director(director_id)
+);
+
+CREATE TABLE PlatformRating (
+    movie_id INT,
+    platform_id INT,
+    rating FLOAT,
+    CHECK (rating >= 0 AND rating <= 10),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
+    FOREIGN KEY (platform_id) REFERENCES RatingPlatform(platform_id)
+);
+
+CREATE TABLE Watch (
+    Username VARCHAR(255),
+    ID INT,
+    movie_ID INT,
+    watch_times INT,
+    FOREIGN KEY (Username) REFERENCES User(Username),
+    FOREIGN KEY (ID) REFERENCES Watchlist(ID),
+    FOREIGN KEY (movie_ID) REFERENCES Movie(movie_id)
+);
+
+CREATE TABLE Friend (
+    username1 VARCHAR(255),
+    username2 VARCHAR(255),
+    FOREIGN KEY (username1) REFERENCES User(Username),
+    FOREIGN KEY (username2) REFERENCES User(Username)
+);
