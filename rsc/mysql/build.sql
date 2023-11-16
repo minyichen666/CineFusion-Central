b0b8f252a73de6d5530ba69f591d73722e444884
“minyichen666” <m770706946@gmail.com>
2023-11-16 15:25:38 -0500

    load dataset into tables

--- rsc/mysql/build.sql
+++ rsc/mysql/build.sql
@@ -1,32 +1,76 @@
 DROP DATABASE movie_db;
 CREATE DATABASE movie_db;
 USE movie_db;
+
+CREATE TABLE NetflixTitles (
+    show_id VARCHAR(255) PRIMARY KEY,
+    type VARCHAR(50),
+    title VARCHAR(255),
+    director VARCHAR(255),
+    cast VARCHAR(1000),
+    country VARCHAR(255),
+    date_added VARCHAR(255),
+    release_year INT,
+    rating VARCHAR(50),
+    duration VARCHAR(100),
+    listed_in VARCHAR(255),
+    description TEXT
+);
+
+LOAD DATA INFILE '/var/lib/mysql-files/netflix_titles.csv'
+INTO TABLE NetflixTitles
+FIELDS TERMINATED BY ','
+ENCLOSED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
 CREATE TABLE Movie (
     movie_id INT PRIMARY KEY NOT NULL,
     type VARCHAR(255),
     title VARCHAR(255),
     country VARCHAR(255),
     date_added DATE,
-    releasing_year INT,
+    release_year INT,
     duration INT,
-    CHECK (YEAR(date_added) >= releasing_year),
+    CHECK (YEAR(date_added) >= release_year),
     CHECK (duration > 0 AND duration <= 600)
 );
+
+INSERT INTO Movie (movie_id, type, title, country, date_added, release_year, duration)
+SELECT show_id, type, title, country, date_added, release_year, duration
+FROM NetflixTitles;
 
 CREATE TABLE Actor (
     actor_id INT PRIMARY KEY NOT NULL,
     actor_name VARCHAR(255) NOT NULL
 );
 
+INSERT INTO Actor (actor_id, actor_name)
+SELECT DISTINCT actor_id, actor_name
+FROM (
+    SELECT show_id, actor_id, actor_name
+    FROM NetflixTitles
+) AS ActorData;
+
 CREATE TABLE Director (
     director_id INT PRIMARY KEY NOT NULL,
     director_name VARCHAR(255) NOT NULL
 );
 
+INSERT INTO Director (director_id, director_name)
+SELECT DISTINCT director_id, director_name
+FROM (
+    SELECT show_id, director_id, director_name
+    FROM NetflixTitles
+) AS DirectorData;
+
 CREATE TABLE RatingPlatform (
     platform_id INT PRIMARY KEY NOT NULL,
     platform_name VARCHAR(255) NOT NULL
 );
+
+INSERT INTO RatingPlatform (platform_id, platform_name)
+VALUES (1, 'Netflix');
 
 CREATE TABLE User (
     Username VARCHAR(255) PRIMARY KEY NOT NULL,
@@ -44,7 +88,6 @@ CREATE TABLE Watchlist (
     UNIQUE (ID)
 );
 
--- Define relation sets
 CREATE TABLE ActsIn (
     movie_id INT,
     actor_id INT,
@@ -52,12 +95,20 @@ CREATE TABLE ActsIn (
     FOREIGN KEY (actor_id) REFERENCES Actor(actor_id)
 );
 
+INSERT INTO ActsIn (movie_id, actor_id)
+SELECT show_id, actor_id
+FROM NetflixTitles;
+
 CREATE TABLE Directs (
     movie_id INT,
     director_id INT,
     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
     FOREIGN KEY (director_id) REFERENCES Director(director_id)
 );
+
+INSERT INTO Directs (movie_id, director_id)
+SELECT show_id, director_id
+FROM NetflixTitles;
 
 CREATE TABLE PlatformRating (
     movie_id INT,
@@ -67,6 +118,10 @@ CREATE TABLE PlatformRating (
     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
     FOREIGN KEY (platform_id) REFERENCES RatingPlatform(platform_id)
 );
+
+INSERT INTO PlatformRating (movie_id, platform_id, rating)
+SELECT show_id, 1, CAST(rating AS DECIMAL(4, 2))
+FROM NetflixTitles;
 
 CREATE TABLE Watch (
     Username VARCHAR(255),
