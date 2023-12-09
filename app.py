710b426b0a26854ce0d9aa1a3e51d68cca2be0e8
Minyi Chen <43757889+minyichen666@users.noreply.github.com>
2023-12-08 20:29:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Set a secret key for session management
 
 db_config = {
-    # 'host': 'cinefusion-central-mysql',  # If Flask isn't running in a Docker container, use Docker's IP here
-    'host': 'localhost',
+    'host': 'cinefusion-central-mysql',  # If Flask isn't running in a Docker container, use Docker's IP here
+    # 'host': 'localhost',
     'port': 3306,
     'database': 'movie_db',
     'user': 'root',
